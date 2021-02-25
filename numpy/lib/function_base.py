a226c76691c8a49b0531653502bbfc0050d4b139
ZEHUA GUO <zehuag@kth.se>
2021-02-25 18:40:47 +0100

    From 26 to 15. Done with issue #10

--- numpy/lib/function_base.py
+++ numpy/lib/function_base.py
@@ -977,15 +977,55 @@ def gradient(f, *varargs, axis=None, edge_order=1):
     """
     f = np.asanyarray(f)
     N = f.ndim  # number of dimensions
-
-    if axis is None:
-        axes = tuple(range(N))
-    else:
-        axes = _nx.normalize_axis_tuple(axis, N)
-
+    def get_axes(axis,N):
+        if axis is None:
+            return tuple(range(N))
+        else:
+            return  _nx.normalize_axis_tuple(axis, N)
+    #if axis is None:
+    #    axes = tuple(range(N))
+    #else:
+    #    axes = _nx.normalize_axis_tuple(axis, N)
+    axes =get_axes(axis,N)
     len_axes = len(axes)
     n = len(varargs)
-    if n == 0:
+    def get_dx(n,varargs,len_axes):
+        if n == 0:
+            # no spacing argument - use 1 in all axes
+            dx = [1.0] * len_axes
+            return dx
+        elif n == 1 and np.ndim(varargs[0]) == 0:
+            # single scalar for all axes
+            dx = varargs * len_axes
+            return dx
+        elif n == len_axes:
+            # scalar or 1d array for each axis
+            dx = list(varargs)
+            for i, distances in enumerate(dx):
+                distances = np.asanyarray(distances)
+
+                if distances.ndim == 0:
+                    continue
+                elif distances.ndim != 1:
+                    raise ValueError("distances must be either scalars or 1d")
+                if len(distances) != f.shape[axes[i]]:
+                    raise ValueError("when 1d, distances must match "
+                                     "the length of the corresponding dimension")
+                if np.issubdtype(distances.dtype, np.integer):
+                    # Convert numpy integer types to float64 to avoid modular
+                    # arithmetic in np.diff(distances).
+                    distances = distances.astype(np.float64)
+                diffx = np.diff(distances)
+                # if distances are constant reduce to the scalar case
+                # since it brings a consistent speedup
+                if (diffx == diffx[0]).all():
+                    diffx = diffx[0]
+                dx[i] = diffx
+            return dx
+        else:
+            raise TypeError("invalid number of arguments")
+    dx=get_dx(n,varargs,len_axes)
+    """if n == 0:
         # no spacing argument - use 1 in all axes
         dx = [1.0] * len_axes
     elif n == 1 and np.ndim(varargs[0]) == 0:
@@ -1014,7 +1054,7 @@ def gradient(f, *varargs, axis=None, edge_order=1):
                 diffx = diffx[0]
             dx[i] = diffx
     else:
-        raise TypeError("invalid number of arguments")
+        raise TypeError("invalid number of arguments")"""
 
     if edge_order > 2:
         raise ValueError("'edge_order' greater than 2 not supported")
